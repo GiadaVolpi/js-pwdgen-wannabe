7446da47c7abff79193c131e3024cf9561b59cdd
Gi@da <giadavolpi8@gmail.com>
2019-11-13 14:24:17 +0100

    creata password

---
+++ main.js
@@ -0,0 +1,8 @@
+var nome = prompt ("Scrivi il tuo nome");
+console.log (nome);
+var cognome = prompt ("Scrivi il tuo cognome");
+console.log (cognome);
+var colore = prompt ("Scrivi il tuo colore preferito");
+console.log (colore);
+var numero = 19
+document.getElementById ("password").innerHTML = nome + cognome + colore + numero;
